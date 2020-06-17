c8d2f59d207d565ba749e1c2e4feba6b814ee6c2
Arsalan Karimzad <karimzadarsalan@gmail.com>
2020-06-17 15:47:45 +0430

    requirements added

--- web/models.py
+++ web/models.py
@@ -6,7 +6,21 @@ from django.contrib.auth.models import User
 
 class Expense(models.Model):
     text = models.CharField(max_length=255)
-    date = models.DateField()
+    date = models.DateField(auto_now_add=True)
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
+
     pass
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
